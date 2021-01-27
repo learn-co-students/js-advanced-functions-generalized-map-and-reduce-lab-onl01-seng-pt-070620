f6ae18f21b29716153f4642cb630dcbf8966355c
astanley74 <stanley.377@buckeyemail.osu.edu>
2021-01-26 19:06:53 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your functions here
+function map(array, func) {
+    let newArray = [];
+
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(func(array[i]))
+    }
+    return newArray
+}
+
+function reduce(array, func, start) {
+    let result = (!!start) ? start : array[0]
+    let i = (!!start) ? 0 : 1
+    for (; i < array.length; i++) {
+        result = func(array[i], result)
+    }
+    return result
+}
