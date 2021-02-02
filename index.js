7d7460c808895f67415aa68dbacea64e5d94cdf9
katiethile <lethikatie@gmail.com>
2021-02-02 15:52:24 -0800

    Done.

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ function map(src, cb) {
   }
 
   function reduce(src, cb, start){
-    let total = (!!start) ? start : src[0]
-    let i = (!!start) ? 0 : 1
+    let total = start ? start : src[0]
+    let i = start ? 0 : 1
   
     for (; i < src.length; i++) {
       total = cb(src[i], total)
