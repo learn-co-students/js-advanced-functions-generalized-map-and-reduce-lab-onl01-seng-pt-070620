8f4c1581b641ddcd0afa856c7a8705854aae4339
gracenak <gracenak@gmail.com>
2021-02-03 20:18:36 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your functions here
+function map(sourceArray, a) {
+    let result = []
+    for (let i = 0; i < sourceArray.length; i++) {
+        result.push(a(sourceArray[i]))
+    }
+    return result
+}
+
+function reduce(sourceArray, a, startingPoint) {
+    let total = !!startingPoint ? startingPoint : sourceArray[0]
+
+    if (startingPoint) {
+    for (let i = 0; i < sourceArray.length; i++) {
+        total = a(sourceArray[i], total)
+        }
+    } else {
+        for (let i = 1; i < sourceArray.length; i++) {
+            total = a(sourceArray[i], total)
+        }    
+    }
+    return total
+}
