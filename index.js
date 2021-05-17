07980e3fa4a70b9d8c08db8c82f81e09c9c4ea32
julieconnors <juliana.g.connors@gmail.com>
2021-05-17 13:53:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Add your functions here
+function map(source, callBack) {
+    let newList = [];
+    for (let i = 0; i < source.length; i++) {
+
+        newList.push(callBack(source[i]));
+    }
+    return newList;
+}
+
+function reduce(source, callBack, start) {
+    let result = start ? start : source[0]
+    let i = start ? 0 : 1
+
+    for (; i < source.length; i++) {
+         result = callBack(source[i], result);
+    }
+
+    return result;
+}
